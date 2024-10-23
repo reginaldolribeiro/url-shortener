210a32231df30335b73082bfab2b4286b4bbaaac
reginaldo <reginaldolribeiro@gmail.com>
2024-10-23 16:40:52 -0300

    Fix mvn problems

--- src/main/java/com/reginaldolribeiro/url_shortener/adapter/configuration/RedisConfig.java
+++ src/main/java/com/reginaldolribeiro/url_shortener/adapter/configuration/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.reginaldolribeiro.url_shortener.adapter.configuration;
 
-import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
