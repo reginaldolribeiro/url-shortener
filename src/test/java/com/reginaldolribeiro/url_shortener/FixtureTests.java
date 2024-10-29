e0044747884496799e9284dca5d48a7680e6a96e
reginaldo <reginaldolribeiro@gmail.com>
2024-10-29 11:54:40 -0300

    Refactor database repositories; Add unit tests;

--- src/test/java/com/reginaldolribeiro/url_shortener/FixtureTests.java
+++ src/test/java/com/reginaldolribeiro/url_shortener/FixtureTests.java
@@ -95,15 +95,4 @@ public class FixtureTests {
         );
     }
 
-    public static UserEntity getUserEntity(){
-        return new UserEntity(
-                UUID.randomUUID().toString(),
-                DEFAULT_USER_NAME,
-                DEFAULT_USER_EMAIL,
-                LocalDateTime.now(Clock.systemUTC()),
-                LocalDateTime.now(Clock.systemUTC()),
-                true
-        );
-    }
-
 }
