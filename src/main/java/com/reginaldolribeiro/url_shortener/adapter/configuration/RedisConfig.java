8b43e192721951801eaf2dc293a38afa874d06e2
reginaldo <reginaldolribeiro@gmail.com>
2024-11-04 10:46:23 -0300

    Add dockerfile; Change docker-compose.yml;

--- src/main/java/com/reginaldolribeiro/url_shortener/adapter/configuration/RedisConfig.java
+++ src/main/java/com/reginaldolribeiro/url_shortener/adapter/configuration/RedisConfig.java
@@ -1,12 +1,14 @@
 package com.reginaldolribeiro.url_shortener.adapter.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -17,10 +19,17 @@ import java.time.Duration;
 
 @Configuration
 @EnableCaching
+@Slf4j
 public class RedisConfig {
 
     @Value("${spring.redis.ttl}")
     private int springRedisTtl;
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
 
     private final ObjectMapper objectMapper;
 
@@ -30,13 +39,24 @@ public class RedisConfig {
 //        this.objectMapper.registerModule(new JavaTimeModule()); // Ensure module is registered
     }
 
+//    @Bean
+//    public LettuceConnectionFactory redisConnectionFactory() {
+//        var connectionFactory = new LettuceConnectionFactory();
+//        log.info("***** [LettuceConnectionFactory] - Starting REDIS with host: {} and port: {}", connectionFactory.getHostName(), connectionFactory.getPort());
+//        return connectionFactory;
+//    }
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        var config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        var connectionFactory = new LettuceConnectionFactory(config);
+        log.info("***** [LettuceConnectionFactory] - Starting REDIS with host: {} and port: {}", connectionFactory.getHostName(), connectionFactory.getPort());
+        return connectionFactory;
     }
 
     @Bean
     public RedisTemplate<String, Object> redisTemplate(LettuceConnectionFactory connectionFactory) {
+        log.info("***** [redisTemplate] - Starting REDIS with host: {} and port: {}", redisHost, redisPort);
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
@@ -46,6 +66,7 @@ public class RedisConfig {
 
     @Bean
     public RedisCacheManager cacheManager(LettuceConnectionFactory connectionFactory) {
+        log.info("***** [cacheManager] - Starting REDIS with host: {} and port: {}", redisHost, redisPort);
         RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
                         new GenericJackson2JsonRedisSerializer(objectMapper))) // Explicitly use the configured ObjectMapper
